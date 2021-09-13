3efc73ff955bb015f1c24ce808bbffd163e2b085
HarryLundblad <90604842+HarryLundblad@users.noreply.github.com>
2021-09-13 10:19:19 +0200

    Added project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
